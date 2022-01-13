bc960a09adfd246f6cda744eba41b7f520085889
alexrmsu <al.magna@alumnos.duoc.cl>
2022-01-13 20:50:17 -0300

    Back button added and random page created

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "/", to: "main#index"
+  get "/main", to: "main#index"
   get "/random", to:"random#index"
-
   # Defines the root path route ("/")
   # root "articles#index"
 end
