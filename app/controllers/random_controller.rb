9196bfc2468d7f0161b8ba0cadd07425daf5932e
alexrmsu <al.magna@alumnos.duoc.cl>
2022-01-14 04:04:39 -0300

    API consume update and working, display added in random page

--- app/controllers/random_controller.rb
+++ app/controllers/random_controller.rb
@@ -3,7 +3,13 @@ class RandomController < ApplicationController
     url = "https://pokeapi.co/api/v2/pokemon/"
     response = HTTParty.get url
     result = JSON.parse response.to_s
-    @name= result['results'][rand(1..20)]['name']
-    puts " nombre es : #{@name}"
+    code = rand(0..19)
+    @name= result['results'][code]['name']
+    @id = code + 1
+    url_skill = "https://pokeapi.co/api/v2/pokemon/#{@id}"
+    response_skill = HTTParty.get url_skill
+    result = JSON.parse response_skill.to_s
+    @skill= result['abilities'][0]['ability']['name']
+    @url_image = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/#{@id}.png"
   end
 end
