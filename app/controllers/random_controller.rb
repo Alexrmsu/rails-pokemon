fe3e15eab39eca3eb7b97ed46e63b626748b1423
alexrmsu <al.magna@alumnos.duoc.cl>
2022-01-14 04:27:54 -0300

    Minor changes (Reformat and cleanup)

--- app/controllers/random_controller.rb
+++ app/controllers/random_controller.rb
@@ -4,12 +4,12 @@ class RandomController < ApplicationController
     response = HTTParty.get url
     result = JSON.parse response.to_s
     code = rand(0..19)
-    @name= result['results'][code]['name']
+    @name = result['results'][code]['name']
     @id = code + 1
     url_skill = "https://pokeapi.co/api/v2/pokemon/#{@id}"
     response_skill = HTTParty.get url_skill
     result = JSON.parse response_skill.to_s
-    @skill= result['abilities'][0]['ability']['name']
+    @skill = result['abilities'][0]['ability']['name']
     @url_image = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/#{@id}.png"
   end
 end
