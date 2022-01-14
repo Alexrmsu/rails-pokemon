572705bb1e83ac224738077a4e817ca290bd9881
alexrmsu <al.magna@alumnos.duoc.cl>
2022-01-14 01:55:08 -0300

    Random pokemon name

--- app/controllers/random_controller.rb
+++ app/controllers/random_controller.rb
@@ -3,7 +3,7 @@ class RandomController < ApplicationController
     url = "https://pokeapi.co/api/v2/pokemon/"
     response = HTTParty.get url
     result = JSON.parse response.to_s
-    @name= result['results']
+    @name= result['results'][rand(1..20)]['name']
     puts " nombre es : #{@name}"
   end
 end
