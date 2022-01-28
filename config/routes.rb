6c6e6afcbbe3db05eceea20831dc6622d5dee630
alexrmsu <al.magna@alumnos.duoc.cl>
2022-01-27 22:05:33 -0300

    map leaflet test

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get "/", to: "main#index"
   get "/main", to: "main#index"
   get "/random", to:"random#index"
+  get "/map", to:"map#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
